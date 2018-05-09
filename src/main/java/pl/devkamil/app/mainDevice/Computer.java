e0d9116ae72f3827131dc9ed27058c55006d2302
devkamil <kamilkarwowski.dev@gmail.com>
2018-05-09 11:46:51 +0200

    change 'findProductByBarCode' method - adding search in the H2 Database

--- src/main/java/pl/devkamil/app/mainDevice/Computer.java
+++ src/main/java/pl/devkamil/app/mainDevice/Computer.java
@@ -1,5 +1,8 @@
 package pl.devkamil.app.mainDevice;
 
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import pl.devkamil.app.database.HibernateUtil;
 import pl.devkamil.app.exceptions.InvalidBarCodeException;
 import pl.devkamil.app.exceptions.ProductNotFoundException;
 import pl.devkamil.app.inputDevices.BarCodeScanner;
@@ -9,6 +12,7 @@ import pl.devkamil.app.model.Product;
 import pl.devkamil.app.outputDevices.LcdDisplay;
 import pl.devkamil.app.outputDevices.Printer;
 
+import javax.persistence.NoResultException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,18 +22,17 @@ public class Computer {
     private final static String EXIT = "exit";
     private final static String EMPTY_BAR_CODE = "";
     private BarCodeScanner barCodeScanner;
-    private Database database;
     private LcdDisplay lcdDisplay;
     private Printer printer;
     private List<Printable> listOfProducts;
-
+    private HibernateUtil hibernateUtil;
 
     public Computer(){
         barCodeScanner = new BarCodeScanner();
-        database = new Database();
         lcdDisplay = new LcdDisplay();
         printer = new Printer();
         listOfProducts = new ArrayList();
+        hibernateUtil = new HibernateUtil();
     }
 
 
@@ -60,7 +63,7 @@ public class Computer {
         showAndPrintResult(listOfProducts);
     }
 
-    private BigDecimal sumOfProducts(List<Printable> listOfProduct) {
+    public BigDecimal sumOfProducts(List<Printable> listOfProduct) {
         BigDecimal sumOfPrice = new BigDecimal("0");
         for(Printable product: listOfProduct){
             Product p = (Product) product;
@@ -69,23 +72,37 @@ public class Computer {
         return sumOfPrice;
     }
 
-    private boolean verifyBarCode(BarCode barCode) throws InvalidBarCodeException {
+    public boolean verifyBarCode(BarCode barCode) throws InvalidBarCodeException {
         if (!EMPTY_BAR_CODE.equals(barCode.getBarCode())){
             return true;
         }
         throw new InvalidBarCodeException();
     }
 
-    private Product findProductByBarCode(BarCode barCode) throws ProductNotFoundException {
-        return database.findProductByBarCode(barCode);
+    public Product findProductByBarCode(BarCode barCode) throws ProductNotFoundException {
+        Product product = new Product();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM Product P WHERE P.barCode.barCode = :barCode";
+            Query query = session.createQuery(hql);
+            query.setParameter("barCode", barCode.getBarCode());
+            product = (Product) query.getSingleResult();
+            session.close();
+        }catch(NoResultException ex){
+            throw new ProductNotFoundException(barCode);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return product;
     }
 
-    private void showAndPrintResult(List<Printable> listPrintable){
+    public void showAndPrintResult(List<Printable> listPrintable){
         BigDecimal sumOfProducts = sumOfProducts(listPrintable);
         printer.print(listPrintable);
         printer.printSum(sumOfProducts);
         lcdDisplay.showSum(sumOfProducts);
         barCodeScanner.closeInput();
+        HibernateUtil.getSessionFactory().close();
     }
 
 }
