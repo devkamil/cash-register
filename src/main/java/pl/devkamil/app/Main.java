322924eedc82e8a320d962513a9cf2c688c6361a
devkamil <kamilkarwowski.dev@gmail.com>
2018-05-10 00:41:59 +0200

    change 'Main' class to SpringBootApplication main class

--- src/main/java/pl/devkamil/app/Main.java
+++ src/main/java/pl/devkamil/app/Main.java
@@ -1,11 +1,18 @@
 package pl.devkamil.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import pl.devkamil.app.mainDevice.Computer;
 
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args){
-        Computer computer = new Computer();
+
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
+        Computer computer = ctx.getBean(Computer.class);
         computer.run();
     }
 }
