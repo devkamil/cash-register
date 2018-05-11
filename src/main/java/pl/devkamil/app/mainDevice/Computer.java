057e84ca1dd66a79ef8966246aee0e93da87fe43
devkamil <kamilkarwowski.dev@gmail.com>
2018-05-11 13:19:46 +0200

    create 'ProductService' class and transfer all methods to it from 'Computer' class (without 'run()' method)

--- src/main/java/pl/devkamil/app/mainDevice/Computer.java
+++ src/main/java/pl/devkamil/app/mainDevice/Computer.java
@@ -4,16 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.devkamil.app.exceptions.InvalidBarCodeException;
 import pl.devkamil.app.exceptions.ProductNotFoundException;
-import pl.devkamil.app.inputDevices.BarCodeScanner;
 import pl.devkamil.app.model.BarCode;
 import pl.devkamil.app.model.Printable;
 import pl.devkamil.app.model.Product;
 import pl.devkamil.app.outputDevices.LcdDisplay;
-import pl.devkamil.app.outputDevices.Printer;
-import pl.devkamil.app.repository.ProductRepository;
+import pl.devkamil.app.service.ProductService;
 
-
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,38 +17,32 @@ import java.util.List;
 public class Computer {
 
     private final static String EXIT = "exit";
-    private final static String EMPTY_BAR_CODE = "";
-    private List<Printable> listOfProducts = new ArrayList<>();
-
-    @Autowired
-    private BarCodeScanner barCodeScanner;
+    private List<Printable> listOfProducts;
 
     @Autowired
     private LcdDisplay lcdDisplay;
 
     @Autowired
-    private Printer printer;
-
-    @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
 
 
     public void run(){
 
+        listOfProducts = new ArrayList<>();
         BarCode scannedBarCode = null;
 
         while(!EXIT.equals(scannedBarCode)){
 
             lcdDisplay.showInputMessage("Enter the bar code: ");
-            scannedBarCode = barCodeScanner.scan();
+            scannedBarCode = productService.barCodeScan();
 
             if(EXIT.equals(scannedBarCode.getBarCode())) {
                 break;
             }
 
             try {
-                verifyBarCode(scannedBarCode);
-                Product product = findProductByBarCode(scannedBarCode);
+                productService.verifyBarCode(scannedBarCode);
+                Product product = productService.findProductByBarCode(scannedBarCode);
                 listOfProducts.add(product);
                 lcdDisplay.showMessage(product.getName(), product.getPrice());
             }catch(InvalidBarCodeException ex){
@@ -61,39 +51,6 @@ public class Computer {
                 lcdDisplay.showErrorMessage(ex.getMessage());
             }
         }
-        showAndPrintResult(listOfProducts);
+        productService.showAndPrintResult(listOfProducts);
     }
-
-    public BigDecimal sumOfProducts(List<Printable> listOfProduct) {
-        BigDecimal sumOfPrice = new BigDecimal("0");
-        for(Printable product: listOfProduct){
-            Product p = (Product) product;
-            sumOfPrice = sumOfPrice.add(p.getPrice());
-        }
-        return sumOfPrice;
-    }
-
-    public boolean verifyBarCode(BarCode barCode) throws InvalidBarCodeException {
-        if (!EMPTY_BAR_CODE.equals(barCode.getBarCode())){
-            return true;
-        }
-        throw new InvalidBarCodeException();
-    }
-
-    public Product findProductByBarCode(BarCode barCode) throws ProductNotFoundException {
-        Product product = productRepository.findByBarCodeBarCode(barCode.getBarCode());
-        if(product != null) {
-            return product;
-        }
-        throw new ProductNotFoundException(barCode);
-    }
-
-    public void showAndPrintResult(List<Printable> listPrintable){
-        BigDecimal sumOfProducts = sumOfProducts(listPrintable);
-        printer.print(listPrintable);
-        printer.printSum(sumOfProducts);
-        lcdDisplay.showSum(sumOfProducts);
-        barCodeScanner.closeInput();
-    }
-
 }
---
+++ src/main/java/pl/devkamil/app/service/ProductService.java
@@ -0,0 +1,69 @@
+package pl.devkamil.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.devkamil.app.exceptions.InvalidBarCodeException;
+import pl.devkamil.app.exceptions.ProductNotFoundException;
+import pl.devkamil.app.inputDevices.BarCodeScanner;
+import pl.devkamil.app.model.BarCode;
+import pl.devkamil.app.model.Printable;
+import pl.devkamil.app.model.Product;
+import pl.devkamil.app.outputDevices.LcdDisplay;
+import pl.devkamil.app.outputDevices.Printer;
+import pl.devkamil.app.repository.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final static String EMPTY_BAR_CODE = "";
+
+    @Autowired
+    private BarCodeScanner barCodeScanner;
+
+    @Autowired
+    private LcdDisplay lcdDisplay;
+
+    @Autowired
+    private Printer printer;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public BarCode barCodeScan(){
+        return barCodeScanner.scan();
+    }
+
+    public Product findProductByBarCode(BarCode barCode) throws ProductNotFoundException {
+        Product product = productRepository.findByBarCodeBarCode(barCode.getBarCode());
+        if(product != null) {
+            return product;
+        }
+        throw new ProductNotFoundException(barCode);
+    }
+
+    public BigDecimal sumOfProducts(List<Printable> listOfProduct) {
+        BigDecimal sumOfPrice = new BigDecimal("0");
+        for(Printable product: listOfProduct){
+            Product p = (Product) product;
+            sumOfPrice = sumOfPrice.add(p.getPrice());
+        }
+        return sumOfPrice;
+    }
+
+    public boolean verifyBarCode(BarCode barCode) throws InvalidBarCodeException {
+        if (!EMPTY_BAR_CODE.equals(barCode.getBarCode())){
+            return true;
+        }
+        throw new InvalidBarCodeException();
+    }
+
+    public void showAndPrintResult(List<Printable> listPrintable){
+        BigDecimal sumOfProducts = sumOfProducts(listPrintable);
+        printer.print(listPrintable);
+        printer.printSum(sumOfProducts);
+        lcdDisplay.showSum(sumOfProducts);
+        barCodeScanner.closeInput();
+    }
+}
