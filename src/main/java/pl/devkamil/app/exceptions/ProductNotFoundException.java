63af0a6adab0e2ccedc8d5c765efdc31bcc5679b
devkamil <kamilkarwowski.dev@gmail.com>
2018-05-12 03:06:34 +0200

    some fixes in 'exceptions' classes

--- src/main/java/pl/devkamil/app/exceptions/ProductNotFoundException.java
+++ src/main/java/pl/devkamil/app/exceptions/ProductNotFoundException.java
@@ -4,12 +4,12 @@ import pl.devkamil.app.model.BarCode;
 
 public class ProductNotFoundException extends Exception {
 
-    private static final String MESSAGE = "Product with %s barcode doesn't exist in database";
+    private static final String NOT_FOUND_EXCEPTION_MESSAGE = "Product with %s barcode doesn't exist in database";
     private BarCode barCode;
 
 
     public String getMessage() {
-        return String.format(MESSAGE, barCode.getBarCode());
+        return String.format(NOT_FOUND_EXCEPTION_MESSAGE, barCode.getBarCode());
     }
 
     public ProductNotFoundException(BarCode barCode) {
