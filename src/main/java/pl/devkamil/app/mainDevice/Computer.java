5f1136ce8dd1764e7e298350020f17618f0f1b76
devkamil <kamilkarwowski.dev@gmail.com>
2018-05-18 23:34:28 +0200

    there is only 'run()' method

--- src/main/java/pl/devkamil/app/mainDevice/Computer.java
+++ src/main/java/pl/devkamil/app/mainDevice/Computer.java
@@ -2,12 +2,11 @@ package pl.devkamil.app.mainDevice;
 
 import pl.devkamil.app.exceptions.InvalidBarCodeException;
 import pl.devkamil.app.exceptions.ProductNotFoundException;
-import pl.devkamil.app.inputDevices.BarCodeScanner;
 import pl.devkamil.app.model.BarCode;
 import pl.devkamil.app.model.Printable;
 import pl.devkamil.app.model.Product;
 import pl.devkamil.app.outputDevices.LcdDisplay;
-import pl.devkamil.app.outputDevices.Printer;
+import pl.devkamil.app.service.ProductService;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -16,39 +15,34 @@ import java.util.List;
 public class Computer {
 
     private final static String EXIT = "exit";
-    private final static String EMPTY_BAR_CODE = "";
-    private BarCodeScanner barCodeScanner;
-    private Database database;
-    private LcdDisplay lcdDisplay;
-    private Printer printer;
     private List<Printable> listOfProducts;
+    private ProductService productService;
+    private LcdDisplay lcdDisplay;
 
 
     public Computer(){
-        barCodeScanner = new BarCodeScanner();
-        database = new Database();
+        productService = new ProductService();
         lcdDisplay = new LcdDisplay();
-        printer = new Printer();
-        listOfProducts = new ArrayList();
     }
 
 
     public void run(){
 
+        listOfProducts = new ArrayList();
         BarCode scannedBarCode = null;
 
         while(!EXIT.equals(scannedBarCode)){
 
             lcdDisplay.showInputMessage();
-            scannedBarCode = barCodeScanner.scan();
+            scannedBarCode = productService.barCodeScan();
 
             if(EXIT.equals(scannedBarCode.getBarCode())) {
                 break;
             }
 
             try {
-                verifyBarCode(scannedBarCode);
-                Product product = findProductByBarCode(scannedBarCode);
+                productService.verifyBarCode(scannedBarCode);
+                Product product = productService.findProductByBarCode(scannedBarCode);
                 listOfProducts.add(product);
                 lcdDisplay.showOneProductMessage(product.getName(), product.getPrice());
             }catch(InvalidBarCodeException ex){
@@ -57,35 +51,9 @@ public class Computer {
                 lcdDisplay.showErrorMessage(ex.getMessage());
             }
         }
-        showAndPrintResult(listOfProducts);
-    }
-
-    public BigDecimal sumOfProducts(List<Printable> listOfProduct) {
-        BigDecimal sumOfPrice = new BigDecimal("0");
-        for(Printable product: listOfProduct){
-            Product p = (Product) product;
-            sumOfPrice = sumOfPrice.add(p.getPrice());
-        }
-        return sumOfPrice;
-    }
-
-    public boolean verifyBarCode(BarCode barCode) throws InvalidBarCodeException {
-        if (!EMPTY_BAR_CODE.equals(barCode.getBarCode())){
-            return true;
-        }
-        throw new InvalidBarCodeException();
-    }
-
-    public Product findProductByBarCode(BarCode barCode) throws ProductNotFoundException {
-        return database.findProductByBarCode(barCode);
-    }
-
-    private void showAndPrintResult(List<Printable> listPrintable){
-        BigDecimal sumOfProducts = sumOfProducts(listPrintable);
-        printer.print(listPrintable);
-        printer.printSum(sumOfProducts);
+        BigDecimal sumOfProducts = productService.sumOfProducts(listOfProducts);
         lcdDisplay.showSum(sumOfProducts);
-        barCodeScanner.closeInput();
+        productService.printResult(listOfProducts, sumOfProducts);
+        productService.closeInput();
     }
-
 }
